47590e9faae281971ca331cfc543037cb91d434f
Abhyudaytripathy <grimnightwork@gmail.com>
2024-12-05 00:01:20 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -212,6 +212,25 @@ function ProductList() {
             ]
         }
     ];
+    <div className="product-grid">
+        {plantsArray.map((category, index) => (
+        <div key={index}>
+            <h1><div>{category.category}</div></h1>
+            <div className="product-list">
+                {category.plants.map((plant, plantIndex) => (
+                <div className="product-card" key={plantIndex}>
+                    <img className="product-image" src={plant.image} alt={plant.name} />
+                    <div className="product-title">{plant.name}</div>
+                    <div className="product-description">{plant.description}</div>
+                    <div className="product-cost">{plant.cost}</div>
+                    {/*Similarly like the above plant.name show other details like description and cost*/}
+                    <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                </div>
+                ))}
+            </div>
+        </div>
+        ))}
+    </div>
    const styleObj={
     backgroundColor: '#4CAF50',
     color: '#fff!important',
